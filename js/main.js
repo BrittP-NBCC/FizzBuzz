671a804f2c46ee31d3d28022b43b85e33166a239
Brittany <britt.goguen@gmail.com>
2021-08-18 19:08:27 -0300

    app completed

--- js/main.js
+++ js/main.js
@@ -76,10 +76,19 @@ function displayData(fbArray){
 
         //select tds and put in an array
         let rowCols = tableRow.querySelectorAll("td");
+        rowCols[0].classList.add(fbArray[index]);
         rowCols[0].textContent = fbArray[index];
+
+        rowCols[1].classList.add(fbArray[index+1]);
         rowCols[1].textContent = fbArray[index+1];
+
+        rowCols[2].classList.add(fbArray[index+2]);
         rowCols[2].textContent = fbArray[index+2];
+
+        rowCols[3].classList.add(fbArray[index+3]);
         rowCols[3].textContent = fbArray[index+3];
+
+        rowCols[4].classList.add(fbArray[index+4]);
         rowCols[4].textContent = fbArray[index+4];
 
         tableBody.appendChild(tableRow);
